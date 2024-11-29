b18be32d4d27021e7cec7201e40a4e4de8e98f99
overjjang99 <overjjang99@gmail.com>
2024-11-29 11:39:40 +0900

    포트 없을 시 포트 바꾸도록, 점자 입력 시 자동 sleep수행, 정지 기능 추가

--- server.py
+++ server.py
@@ -8,32 +8,52 @@ import time
 app = Flask(__name__)
 
 # 아두이노 직렬 포트 설정 (라즈베리파이에 연결된 아두이노)
-arduino1 = serial.Serial('/dev/ttyACM0', 9600)  # 아두이노 1(점자블록)
-arduino2 = serial.Serial('/dev/ttyUSB0', 9600)  # 아두이노 2(모터)
+try:
+    arduino1 = serial.Serial('/dev/ttyACM0', 9600)  # 아두이노 1(점자블록)
+except serial.SerialException:
+    arduino1 = serial.Serial('/dev/ttyACM1', 9600)  # 포트가 일치하지 않으면 대체 포트 사용
+
+try:
+    arduino2 = serial.Serial('/dev/ttyUSB0', 9600)  # 아두이노 2(모터)
+except serial.SerialException:
+    arduino2 = serial.Serial('/dev/ttyUSB1', 9600)  # 포트가 일치하지 않으면 대체 포트 사용
+
+stop_flag = False
 
 def send_braille_signal(data):
     signal = data
 
     arduino1.write(signal.encode())
-    time.sleep(0.5)
+    print("출력:"+signal)
+    time.sleep(len(signal) * 0.2)
     return signal
 
 def dirve(data):
     route = data.get('signal')
+    global stop_flag
+    stop_flag = False
     # 목적지에 따라 아두이노로 보낼 신호를 변수에 넣기 형식: 배열 [[방향,시간(초)]]
     # 정지:1 전진:2 후진:3 왼쪽:4 오른쪽:5
     routes = {
         '농협': [['2', 20], ['1', 5], ['4', 5], ['1', 5], ['5', 5], ['1', 5], ['2', 20]],
+
         '한아름실': [['2', 5], ['1', 5], ['4', 5], ['1', 5], ['2', 35], ['1', 5], ['4', 5], ['1', 5], ['2', 5], ['1', 5]],
+
         '위클래스': [['2', 5], ['1', 5], ['4', 5], ['1', 5], ['2', 15], ['1', 5], ['4', 5], ['1', 5], ['2', 5], ['1', 5]],
+
         '미술실': [['2', 5], ['1', 5], ['4', 5], ['1', 5], ['2', 25], ['1', 5], ['4', 5], ['1', 5], ['2', 5], ['1', 5]],
+
         '음악실': [['2', 5], ['1', 5], ['4', 5], ['1', 5], ['2', 25], ['1', 5], ['5', 5], ['1', 5], ['2', 5], ['1', 5]],
+
         '화장실(중앙)': [['2', 10], ['1', 5], ['4', 5], ['1', 5], ['2', 15], ['1', 5], ['5', 5], ['1', 5], ['2', 10],
                     ['1', 5]],
-        '2학년 교사실': [['2', 5], ['1', 5], ['5', 5], ['1', 5], ['2', 10], ['1', 5], ['5', 5], ['1', 5], ['2', 20],
+
+        '2학년 교무실': [['2', 5], ['1', 5], ['5', 5], ['1', 5], ['2', 10], ['1', 5], ['5', 5], ['1', 5], ['2', 20],
                     ['1', 5], ['5', 5], ['2', 5], ['1', 5]],
+
         '열린토론 학습실': [['2', 10], ['1', 5], ['4', 5], ['1', 5], ['2', 15], ['1', 5], ['5', 5], ['1', 5], ['2', 10],
                      ['1', 5], ['5', 5], ['1', 5], ['2', 10], ['4', 5], ['1', 5], ['2', 5], ['1', 5]],
+
         '진로활동실': [['2', 5], ['1', 5], ['5', 5], ['1', 5], ['2', 10], ['1', 5], ['5', 5], ['1', 5], ['2', 5], ['1', 5],
                   ['4', 5], ['1', 5]]
     }
@@ -43,29 +63,29 @@ def dirve(data):
     else:
         return jsonify({"message": "Invalid destination!"}), 418
     for i in range(len(route)):
+        if stop_flag:
+            send_braille_signal("정지 명령 수신")
+            arduino2.write('1'.encode())
+            return jsonify({"message": "정지 명령 수신"}), 200
         if route[i][0] == '1':
-            arduino1.write("정지합니다".encode())
-            time.sleep(5)
+            send_braille_signal("정지")
         elif route[i][0] == '2':
-            arduino1.write("전진합니다".encode())
-            time.sleep(5)
-
+            send_braille_signal("전진")
         elif route[i][0] == '3':
-            arduino1.write("후진합니다".encode())
-            time.sleep(5)
-
+            send_braille_signal("후진")
         elif route[i][0] == '4':
-            arduino1.write("왼쪽으로 이동합니다".encode())
-            time.sleep(5)
-
+            send_braille_signal("좌회전")
         elif route[i][0] == '5':
-            arduino1.write("오른쪽으로 이동합니다".encode())
-            time.sleep(5)
-
+            send_braille_signal("우회전")
         else:
-            arduino1.write(route[i][0].encode())
+            send_braille_signal("잘못된 신호 수신, 정지합니다")
+            arduino2.write('1'.encode())
+            return jsonify({"message": "잘못된 신호 수신, 정지합니다"}), 500
+
         arduino2.write(route[i][0].encode())
         time.sleep(route[i][1])
+    send_braille_signal(route+"에 도착하였습니다")
+
 
 # 웹 서버의 POST 요청 처리
 @app.route('/send_signal', methods=['POST'])
@@ -79,10 +99,11 @@ def send_signal():
     if not signal:
         return jsonify({"message": "Signal not found in the request!"}), 418
 
-    send_braille_signal(signal + "으로 출발합니다")
+    send_braille_signal(str(signal) + "으로 출발합니다")
+    jsonify({"message": f"출발 :'{signal}' "}), 202
     print(signal)
     dirve(data)
-    return jsonify({"message": f"Signal '{signal}' sent to Arduino successfully!"}), 200
+    return jsonify({"message": f"'{signal}' 작업완료"}), 200
 
 
     # 아두이노로 신호 보내기
@@ -91,6 +112,14 @@ def send_signal():
     # time.sleep(0.5)  # 잠시 대기
     #
     # return jsonify({"message": f"Signal '{braille}' sent to Arduino successfully!"}), 200
+
+@app.route('/stop', methods=['POST'])
+def stop():
+    arduino2.write('1'.encode())
+    global stop_flag
+    stop_flag = True
+    return jsonify({"message": "정지"}), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
