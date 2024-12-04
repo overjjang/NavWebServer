85c8f15492d2029c7ab9005dfba6216f1134025e
overjjang99 <overjjang99@gmail.com>
2024-12-05 00:27:15 +0900

    chore : code clear

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+# 모듈 설치
 from flask import Flask, request, jsonify
 import serial
 import time
@@ -20,6 +21,7 @@ except serial.SerialException:
 
 stop_flag = False
 
+# 점자 출력 함수
 def send_braille_signal(data):
     signal = data
 
@@ -28,6 +30,7 @@ def send_braille_signal(data):
     time.sleep(len(signal) * 0.2)
     return signal
 
+# 모터 이동 함수
 def dirve(data):
     route = data.get('signal')
     global stop_flag
@@ -61,12 +64,14 @@ def dirve(data):
     if route in routes:
         route = routes[route]
     else:
-        return jsonify({"message": "Invalid destination!"}), 418
+        # 목적지가 일치하지 않을 경우
+        return jsonify({"message": "Invalid destination!"}), 400 # 400: Bad Request
     for i in range(len(route)):
         if stop_flag:
             send_braille_signal("정지 명령 수신")
             arduino2.write('1'.encode())
-            return jsonify({"message": "정지 명령 수신"}), 200
+            return jsonify({"message": "정지 명령 수신"}), 200 # 200: OK
+            #강제중지
         if route[i][0] == '1':
             send_braille_signal("정지")
         elif route[i][0] == '2':
@@ -80,45 +85,41 @@ def dirve(data):
         else:
             send_braille_signal("잘못된 신호 수신, 정지합니다")
             arduino2.write('1'.encode())
-            return jsonify({"message": "잘못된 신호 수신, 정지합니다"}), 500
+            return jsonify({"message": "잘못된 신호 수신, 정지합니다"}), 500 # 500: Internal Server Error
 
         arduino2.write(route[i][0].encode())
         time.sleep(route[i][1])
     send_braille_signal(route+"에 도착하였습니다")
 
+# 라즈베리파이 주소: 10.118.72.44:3000/send_signal
+# 아이피 주소 확인 필수
 
-# 웹 서버의 POST 요청 처리
+# POST로 신호 받아 전체 작동
 @app.route('/send_signal', methods=['POST'])
 def send_signal():
     print(request)
-    data = request.json
+    data = request.json # json 데이터 받기
     print(data)
-    signal = data.get('signal')
+    signal = data.get('signal') # 목적지 데이터
 
     # 입력 신호 검증
     if not signal:
-        return jsonify({"message": "Signal not found in the request!"}), 418
+        return jsonify({"message": "Signal not found in the request!"}), 400 # 400: Bad Request
 
     send_braille_signal(str(signal) + "으로 출발합니다")
-    jsonify({"message": f"출발 :'{signal}' "}), 202
-    print(signal)
-    dirve(data)
-    return jsonify({"message": f"'{signal}' 작업완료"}), 200
+    jsonify({"message": f"출발 :'{signal}' "}), 202 # 202: Accepted(확인용 메시지)
+    print(signal) # 목적지 출력
+    dirve(data) # 목적지로 이동
+    return jsonify({"message": f"'{signal}' 작업완료"}), 200 # 200: OK
 
 
-    # 아두이노로 신호 보내기
-    # print(f"Sending signal '{braille}' to Arduino...")
-    # arduino1.write(braille.encode())  # 신호를 직렬 포트로 전송
-    # time.sleep(0.5)  # 잠시 대기
-    #
-    # return jsonify({"message": f"Signal '{braille}' sent to Arduino successfully!"}), 200
-
+# 강제정지 (작동여부 불명(안하는거같은데))
 @app.route('/stop', methods=['POST'])
 def stop():
     arduino2.write('1'.encode())
     global stop_flag
     stop_flag = True
-    return jsonify({"message": "정지"}), 200
+    return jsonify({"message": "정지"}), 200 # 200: OK
 
 
 if __name__ == '__main__':
